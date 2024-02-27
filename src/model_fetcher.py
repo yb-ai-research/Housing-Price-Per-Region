4d1c25cc8b61ff434fa6979c2d1b7902a66efca2
Youngbin Kim <youngbin.kim.yb@gmail.com>
2024-02-27 08:53:24 -0800

    bug fix

--- src/model_fetcher.py
+++ src/model_fetcher.py
@@ -6,13 +6,13 @@ from sklearn.linear_model import LinearRegression
 def get_model(model_name, random_state=42):
     return {
         "linear_regression": LinearRegression(),
-        "decision_tree_gini": tree.DecisionTreeClassifier(
+        "decision_tree_gini": tree.DecisionTreeRegressor(
             criterion="gini",
             random_state=random_state
         ),
-        "decision_tree_entrophy": tree.DecisionTreeClassifier(
+        "decision_tree_entrophy": tree.DecisionTreeRegressor(
             criterion="entrophy",
             random_state=random_state
         ),
-        "rf": ensemble.RandomForestClassifier(random_state=random_state),
+        "rf": ensemble.RandomForestRegressor(random_state=random_state),
     }[model_name]
