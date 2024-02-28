a6ac6b40696f5ad48fa54bb1642d8a52213373ef
Youngbin Kim <youngbin.kim.yb@gmail.com>
2024-02-28 08:37:25 -0800

    terminal

--- src/train.py
+++ src/train.py
@@ -21,14 +21,8 @@ def get_y(df):
     return df["median_house_value"]
 
 
-def get_X(df, should_fit, pipeline):
-    df = df.drop(labels=["median_house_value", "kfold"], axis="columns")
-    if should_fit:
-        res = pipeline.fit_transform(df)
-    else:
-        res = pipeline.transform(df)
-    # print(f"Features after preprocessing: {pipeline.get_feature_names_out()}")
-    return res
+def get_X(df):
+    return df.drop(labels=["median_house_value", "kfold"], axis="columns")
 
 
 def train(fold_name, model, df_train, df_valid, select_from_model):
@@ -38,27 +32,32 @@ def train(fold_name, model, df_train, df_valid, select_from_model):
     feature_pipeline = preprocessing.get_preprocessing_pipeline()
 
     y_train = get_y(df_train)
-    X_train = get_X(df_train, should_fit=True, pipeline=feature_pipeline)
+    X_train = get_X(df_train)
 
     y_valid = get_y(df_valid)
-    X_valid = get_X(df_valid, should_fit=False, pipeline=feature_pipeline)
+    X_valid = get_X(df_valid)
 
     if select_from_model:
         model_selector = SelectFromModel(model)
-        X_train = model_selector.fit_transform(X_train, y_train)
-        X_valid = model_selector.transform(X_valid)
+        pipeline = make_pipeline(
+            feature_pipeline,
+            model_selector,
+            model
+        )
+    else:
+        pipeline = make_pipeline(
+            feature_pipeline,
+            model
+        )
 
-    model.fit(X_train, y_train)
+    pipeline.fit(X_train, y_train)
 
-    preds = model.predict(X_valid)
+    preds = pipeline.predict(X_valid)
     rmse = metrics.mean_squared_error(y_valid, preds, squared=False)
     print(f"Fold={fold_name}, RMSE={rmse}")
 
-    if select_from_model:
-        print(f"Model Selector Estimator Thresholds: {model_selector.threshold_}")
-
     joblib.dump(
-        model,
+        pipeline,
         os.path.join(config.MODEL_OUTPUT, f"dt_{fold_name}_{fold_name}.bin")
     )
     return rmse
@@ -81,7 +80,7 @@ def main(args):
         display_scores(scores)
 
     else:
-        X = get_X(df, should_fit=True, pipeline=preprocessing.get_preprocessing_pipeline())
+        X = get_X(df)
         y = get_y(df)
         scores = cross_val_score(get_model(model), X=X, y=y,
                                  scoring="neg_mean_squared_error", cv=10)
